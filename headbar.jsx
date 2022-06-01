b9027524924db9016a1e370fb430a53521254a9d
01miaom <63391992+01miaom@users.noreply.github.com>
2022-06-02 01:00:22 +0800

    Update headbar.jsx

--- headbar.jsx
+++ headbar.jsx
@@ -1,223 +1,65 @@
+class File extends React.Component {
+    openwindow(){
+        document.getElementById('cover').style.display='block';
+        document.getElementById('floatwindow').style.display='block';
+    }
+    render() {
+        return (
+        <div class="header">
+                <Setting />
+                <div id="cover" class="cover"></div>
+			<ul class="header_ul">
+				<li class="header_ul_file">File
+					<ul class="header_ul_ul">
+						<li>New</li>
+						<li>Save</li>
+						<li>Import</li>
+                        <li>Export</li>
+                        <li onClick={this.openwindow}>Setting</li>
+					</ul>
+				</li>
 
-//import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js";
-//import { GUI } from "./viewhelper/lib.js";
-
-//import { OrbitControls } from "./viewhelper/OrbitControls.js";
-//import { OrbitControlsGizmo } from "./viewhelper/OrbitControlsGizmo.js";
-
-const {useMatch, useParams, useLocation} = ReactRouterDOM;
-const {BrowserRouter, Routes, Route, Link} = ReactRouterDOM;
-
-class App extends React.Component {
-render() {
-return (
-<> 
-    <React.StrictMode>
-  <BrowserRouter>
-      <div>
-      <Routes>
-      	<Route path="/RGameFrontend/" element={<Ui/>} />//fix
-      </Routes>
-      </div>
-    </BrowserRouter>
-    </React.StrictMode>
-</>
+				<li class="header_ul_edit">Edit
+					<ul class="header_ul_ul">
+						<li>Undo</li>
+						<li>Redo</li>
+						<li>Copy</li>
+                        <li>Paste</li>
+                        <li>Cut</li>
+                        <li>Delete</li>
+					</ul>
+				</li>
+				<li class="header_ul_project">Project
+					<ul class="header_ul_ul">
+						<li>Open</li>
+						<li>Add</li>
+                        <li>Example</li>
+					</ul>
+				</li>
+				<li class="header_ul_run"><svg id="run" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+                </svg>
+                    Run
+				</li>
+			</ul>
+                <img id="ico" src="./cat.png"/>
+		</div>
         );
     }
 }
 
-//export
-class Ui extends React.Component {
+
+class Setting extends React.Component {
+    closewindow(){
+        document.getElementById('cover').style.display='none';
+        document.getElementById('floatwindow').style.display='none';       
+    }
     render() {
         return (
-            <div class="wrapper"> 
-                    <HeadBar/> 
-                    <TopBar />
-                    <LeftBar/>
-                    <MainCanvas />
-                    <RightBar />
-                    <CentreBar />
-                    <DetailBar />
-                    <ButtonBar />
+            <div class="floatwindow" draggable="true" id="floatwindow">
+                <p id="closeWindow" onClick={this.closewindow}>x</p>
             </div>
         );
     }
 }
-
-class HeadBar extends React.Component {
-    render() {
-        return (
-            <div id="headBar">
-                    <File />
-            </div>
-        );
-    }
-}
-
-class TopBar extends React.Component {
-    render() {
-        return (
-            <div id="topBarOut">
-                    <Bar />
-            </div>
-        );
-    }
-}
-
-class MainCanvas extends React.Component {
-    render() {
-        return (
-             <div id= "mainCanvas">
-            </div>
-            
-        );
-    }
-}
-
-class LeftBar extends React.Component {
-    render() {
-        return (
-            <div id="leftBar">
-                    <Shape />
-            </div>
-        );
-    }
-}
-
-class RightBar extends React.Component {
-    render() {
-        return (
-            <div id="rightBar">
-                <SearchBar />
-            </div>
-        );
-    }
-}
-
-class DetailBar extends React.Component {
-    render() {
-        return (
-            <div id="detailBar">
-            </div>
-        );
-    }
-}
-
-class ButtonBar extends React.Component {
-    render() {
-        return (
-            <div id="buttonBar">
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(
-<App name="rgame"/>, document.querySelector("#app"));
-
-/*
-var mesh, renderer, scene, camera, controls, controlsGizmo;
-
-init();
-animate();
-
-function init() {
-
-  // renderer
-     const container = document.getElementById( 'mainCanvas' );
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize( container.clientWidth, container.clientHeight );
-  renderer.setClearColor(new THREE.Color(0x333333));
-  renderer.setPixelRatio( window.devicePixelRatio );
-container.appendChild( renderer.domElement );
-  // scene
-  scene = new THREE.Scene();
-  
-  // camera
-  camera = new THREE.PerspectiveCamera( 45,  container.clientWidth/container.clientHeight , 0.1, 10000 );
-  camera.position.set( 15, 12, 12 );
-
-  // Orbit Controls
-  controls = new OrbitControls( camera, renderer.domElement );
-
-  // Obit Controls Gizmo
-  controlsGizmo = new OrbitControlsGizmo(controls, { size: 100, padding: 8 });
-
-  // Add the Gizmo to the document
-    container.appendChild( controlsGizmo.domElement );
-
-  // ambient light
-  scene.add( new THREE.AmbientLight( 0x222222 ) );
-  
-  // directional light
-  var light = new THREE.DirectionalLight( 0xffffff, 1 );
-  light.position.set( 2,2, 0 );
-  scene.add( light );
-
-  // axes Helper
-  const axesHelper = new THREE.AxesHelper( 15 );
-  scene.add( axesHelper );
-
-  // Grid Helper
-  scene.add(new THREE.GridHelper(10, 10, "#666666", "#222222"));
-
-  // geometry
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  
-  // material
-  var material = new THREE.MeshPhongMaterial( {
-    color: 0x00ffff, 
-    transparent: true,
-    opacity: 0.7,
-  });
-  
-  // mesh
-  mesh = new THREE.Mesh( geometry, material );
-  mesh.position.set(0, 0.5, 0);
-  scene.add( mesh );
-
-  // GUI
-  const gui = new GUI();
-  gui.add(controls, 'enabled').name("Enable Orbit Controls");
-  gui.add(controlsGizmo, 'lock').name("Lock Gizmo");
-  gui.add(controlsGizmo, 'lockX').name("Lock Gizmo's X Axis");
-  gui.add(controlsGizmo, 'lockY').name("Lock Gizmo's Y Axis");
-
-}
-
-function animate() {
-
-  requestAnimationFrame( animate );
-  renderer.render( scene, camera );
-  controls.update();
-
-}
-
-function resize() {
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  camera.aspect = ( window.innerWidth / window.innerHeight );
-  camera.updateProjectionMatrix();
-}
-
-window.onresize = resize;
-
-*/
-/*
-
-window.onresize=function(){
-    renderer.setSize(container.clientWidth, container.clientHeight);
-    let k=container.clientWidth/container.clientHeight;
-    //camera.left=-s*k;
-    //camera.right=s*k;
-    //camera.top=s;
-    //camera.bottom=-s;
-    //
-    camera.aspect=k;
-    camera.updateProjectionMatrix();
-}
-*/
-
-
-
-
 
