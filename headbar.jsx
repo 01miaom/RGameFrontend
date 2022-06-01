a63908df180147ca7053c5252bd0b5947cfcbc77
01miaom <63391992+01miaom@users.noreply.github.com>
2022-06-02 00:18:14 +0800

    Update headbar.jsx

--- headbar.jsx
+++ headbar.jsx
@@ -33,9 +33,13 @@ class File extends React.Component {
 					<ul class="header_ul_ul">
 						<li>Open</li>
 						<li>Add</li>
+                        <li>Example</li>
 					</ul>
 				</li>
-				<li class="header_ul_run">Run
+				<li class="header_ul_run"><svg id="run" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+                </svg>
+                    Run
 				</li>
 			</ul>
                 <img id="ico" src="./cat.png"/>
