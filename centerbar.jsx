2ee105f2a48ff5a070ddf770c489b4a1f07590f7
01miaom <63391992+01miaom@users.noreply.github.com>
2022-06-04 20:33:09 +0800

    Update centerbar.jsx

--- centerbar.jsx
+++ centerbar.jsx
@@ -1,12 +1,30 @@
 class CenterBar extends React.Component {
+    clickButton1(){
+        console.log("a");
+        document.getElementById('animationPage').style.visibility = 'hidden';
+        document.getElementById('consolePage').style.visibility = 'hidden';
+        document.getElementById('assetsPage').style.visibility = 'visible';      
+    }
+    clickButton2(){
+        console.log("b");
+        document.getElementById('assetsPage').style.visibility = 'hidden';
+        document.getElementById('consolePage').style.visibility = 'hidden';
+        document.getElementById('animationPage').style.visibility = 'visible';      
+    }
+    clickButton3(){
+        console.log("c");
+        document.getElementById('animationPage').style.visibility = 'hidden';
+        document.getElementById('assetsPage').style.visibility = 'hidden';
+        document.getElementById('consolePage').style.visibility = 'visible';      
+    }
     render() {
         return (
             <div id="centreBar">
                 <ul class="header_ul">
-				<li class="header_ul_file">
+				<li class="header_ul_file" onClick={this.clickButton1}>
                 <svg id="centericon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder-fill" viewBox="0 0 16 16">
-            <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"/>
-                    </svg>    
+<path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"/>
+</svg>    
                     Assets
 					<ul id="assets" class="header_ul_ul">
 						<li id="assetslist">
@@ -26,7 +44,7 @@ class CenterBar extends React.Component {
   <path d="M12.496 8a4.491 4.491 0 0 1-1.703 3.526L9.497 8.5l2.959-1.11c.027.2.04.403.04.61Z"/>
   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-1 0a7 7 0 1 0-13.202 3.249l1.988-1.657a4.5 4.5 0 0 1 7.537-4.623L7.497 6.5l1 2.5 1.333 3.11c-.56.251-1.18.39-1.833.39a4.49 4.49 0 0 1-1.592-.29L4.747 14.2A7 7 0 0 0 15 8Zm-8.295.139a.25.25 0 0 0-.288-.376l-1.5.5.159.474.808-.27-.595.894a.25.25 0 0 0 .287.376l.808-.27-.595.894a.25.25 0 0 0 .287.376l1.5-.5-.159-.474-.808.27.596-.894a.25.25 0 0 0-.288-.376l-.808.27.596-.894Z"/>
 </svg>
-                            materal</li>
+                            material</li>
                         <li id="assetslist">
                          <svg id="asset" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-music" viewBox="0 0 16 16">
   <path d="M11 6.64a1 1 0 0 0-1.243-.97l-1 .25A1 1 0 0 0 8 6.89v4.306A2.572 2.572 0 0 0 7 11c-.5 0-.974.134-1.338.377-.36.24-.662.628-.662 1.123s.301.883.662 1.123c.364.243.839.377 1.338.377.5 0 .974-.134 1.338-.377.36-.24.662-.628.662-1.123V8.89l2-.5V6.64z"/>
@@ -43,7 +61,7 @@ class CenterBar extends React.Component {
 					</ul>
 				</li>
 
-				<li class="header_ul_edit">
+				<li class="header_ul_edit" onClick={this.clickButton2}>
                     <svg id="centericon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-film" viewBox="0 0 16 16">
             <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm4 0v6h8V1H4zm8 8H4v6h8V9zM1 1v2h2V1H1zm2 3H1v2h2V4zM1 7v2h2V7H1zm2 3H1v2h2v-2zm-2 3v2h2v-2H1zM15 1h-2v2h2V1zm-2 3v2h2V4h-2zm2 3h-2v2h2V7zm-2 3v2h2v-2h-2zm2 3h-2v2h2v-2z"/>
                     </svg>
@@ -51,16 +69,31 @@ class CenterBar extends React.Component {
 					<ul class="header_ul_ul">
 					</ul>
 				</li>
-				<li class="header_ul_project">
+				<li class="header_ul_project" onClick={this.clickButton3}>
                     <svg id="centericon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-terminal" viewBox="0 0 16 16">
             <path d="M6 9a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3A.5.5 0 0 1 6 9zM3.854 4.146a.5.5 0 1 0-.708.708L4.793 6.5 3.146 8.146a.5.5 0 1 0 .708.708l2-2a.5.5 0 0 0 0-.708l-2-2z"/>
             <path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h12z"/>
                     </svg>
                     Console
 				</li>
+                    <li>
+                     <div >
+                <svg id="centersearchicon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                </svg>
+               <input id="centersearch" type="text"
+                                autoComplete="off" 
+                                spellCheck="false" 
+                                role="combobox" 
+                                aria-live="polite" 
+                                placeholder="search: object" 
+                                useRef="search" 
+                                name='content'
+                            />
+                        </div>
+                    </li>  
 			</ul>
             </div>
         );
     }
 }
-
