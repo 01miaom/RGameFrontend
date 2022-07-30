eeae2b41d457545191d9f80910d6bb87cbf69cc6
01miaom <63391992+01miaom@users.noreply.github.com>
2022-07-31 04:31:11 +0800

    Update topbar.jsx

--- topbar.jsx
+++ topbar.jsx
@@ -1,15 +1,17 @@
+
+
 class Bar extends React.Component {
     render() {
         return (
             <div id="topBar">
-                <div class="bar">Project1 <div id="barclose">x</div></div>
-                  <div class="bar">Project2 <div id="barclose">x</div></div>
-                <div class="bar">code.R <div id="barclose">x</div></div>
-                <div class="bar">Scene <div id="barclose">x</div></div>
-               <div class="bar">Project1 <div id="barclose">x</div></div>
-                  <div class="bar">Project2 <div id="barclose">x</div></div>
-                <div class="bar">code.R <div id="barclose">x</div></div>
-                <div class="bar">Scene <div id="barclose">x</div></div>
+                <div class="bar"><div>Project1</div> <div id="barclose">×</div></div>
+                  <div class="bar">Project2 <div id="barclose">×</div></div>
+                <div class="bar">code.R <div id="barclose">×</div></div>
+                <div class="bar">Scene <div id="barclose">×</div></div>
+               <div class="bar">Project1 <div id="barclose">×</div></div>
+                  <div class="bar">Project2 <div id="barclose">×</div></div>
+                <div class="bar">code.R <div id="barclose">×</div></div>
+                <div class="bar">Scene <div id="barclose">×</div></div>
             </div>
         );
     }
