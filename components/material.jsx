2bd43d27f056952cc9b77ea29b6c651616e21fe2
01miaom <63391992+01miaom@users.noreply.github.com>
2022-07-31 04:35:42 +0800

    Add files via upload

---
+++ components/material.jsx
@@ -0,0 +1,10 @@
+function Material() {
+        return (
+            <div id="materialcards" >
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-circle-fill" viewBox="0 0 16 16" style={{color:'#eee'}}>
+                <circle cx="8" cy="8" r="8"/>
+                </svg>
+                <p id="materialname">material</p>
+            </div>
+        );
+}
---
+++ components/video.jsx
@@ -0,0 +1,10 @@
+function Video() {
+        return (
+            <div id="videocards">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-play-btn-fill" viewBox="0 0 16 16" style={{color:'#eee'}}>
+                <path d="M0 12V4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm6.79-6.907A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
+                </svg>
+                <p id="materialname">video</p>
+            </div>
+        );
+}
