e6d57f25030d6c16464ee2805d0729445e4e3d11
01miaom <63391992+01miaom@users.noreply.github.com>
2022-09-30 02:30:20 +0800

    Update topbar.jsx

--- topbar.jsx
+++ topbar.jsx
@@ -1,12 +1,20 @@
-
-
 class Bar extends React.Component {
+     clickButton1(){
+        console.log("change to maincanvas");
+        document.getElementById('codeEditor').style.visibility = 'hidden';
+        document.getElementById('mainCanvas').style.visibility = 'visible';  
+    }
+     clickButton2(){
+        console.log("change to codeeditor");
+        document.getElementById('codeEditor').style.visibility = 'visible';
+        document.getElementById('mainCanvas').style.visibility = 'hidden';  
+    }
     render() {
         return (
             <div id="topBar">
-                <div class="bar"><div>Project1</div> <div id="barclose">×</div></div>
+                <div class="bar"  onClick={this.clickButton1}><div>Project1</div> <div id="barclose">×</div></div>
                   <div class="bar">Project2 <div id="barclose">×</div></div>
-                <div class="bar">code.R <div id="barclose">×</div></div>
+                <div class="bar" onClick={this.clickButton2}>code.R <div id="barclose">×</div></div>
                 <div class="bar">Scene <div id="barclose">×</div></div>
                <div class="bar">Project1 <div id="barclose">×</div></div>
                   <div class="bar">Project2 <div id="barclose">×</div></div>
