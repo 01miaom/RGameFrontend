150040f754e66cea2a46565b4f438d9f1b47e078
01miaom <63391992+01miaom@users.noreply.github.com>
2022-10-01 18:35:46 +0800

    Update headbar.jsx

--- headbar.jsx
+++ headbar.jsx
@@ -7,6 +7,16 @@ class File extends React.Component {
         document.getElementById('cover').style.display='block';
         document.getElementById('uploadwindow').style.display='block';
     }
+    runProgram(){
+        let userCode = document.getElementById('codeFrame').contentWindow.myCodeMirror.getValue();
+        
+        var script = document.createElement('script');
+        script.type = 'module';
+        script.appendChild(document.createTextNode(userCode));
+        
+        document.body.appendChild(script);
+        
+    }
     render() {
         return (
         <div class="header">
@@ -43,7 +53,8 @@ class File extends React.Component {
 				</li>
                 <li class="header_ul_project">Help
                 </li>
-				<li class="header_ul_run"><svg id="run" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+				<li class="header_ul_run" onClick={this.runProgram}>
+		<svg id="run" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
                 <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
                 </svg>
                     Run
