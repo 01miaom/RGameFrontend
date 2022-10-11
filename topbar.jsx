c05f36c735fc531eca7e2772a3382429b20056fa
01miaom <63391992+01miaom@users.noreply.github.com>
2022-10-12 02:53:02 +0800

    Update topbar.jsx

--- topbar.jsx
+++ topbar.jsx
@@ -1,17 +1,31 @@
+
 class Bar extends React.Component {
      clickButton1(){
         console.log("change to maincanvas");
         document.getElementById('codeEditor').style.visibility = 'hidden';
+        document.getElementById('texture').style.visibility = 'hidden';
         document.getElementById('mainCanvas').style.visibility = 'visible';
         document.getElementById('Scene1').style.boxShadow = '0 0 4px white';
          document.getElementById('code1').style.boxShadow = '0 0 0 0 white';
+         document.getElementById('texture1').style.boxShadow = '0 0 0 0 white';
     }
      clickButton2(){
         console.log("change to codeeditor");
         document.getElementById('codeEditor').style.visibility = 'visible';
+        document.getElementById('texture').style.visibility = 'hidden';
         document.getElementById('mainCanvas').style.visibility = 'hidden';
         document.getElementById('code1').style.boxShadow = '0 0 4px white';
          document.getElementById('Scene1').style.boxShadow = '0 0 0 0 white';
+         document.getElementById('texture1').style.boxShadow = '0 0 0 0 white';
+    }
+    clickButton3(){
+        console.log("change to texture");
+        document.getElementById('texture').style.visibility = 'visible';
+        document.getElementById('codeEditor').style.visibility = 'hidden';
+        document.getElementById('mainCanvas').style.visibility = 'hidden';
+        document.getElementById('code1').style.boxShadow = '0 0 0 0 white';
+         document.getElementById('Scene1').style.boxShadow = '0 0 0 0 white';
+        document.getElementById('texture1').style.boxShadow = '0 0 4px white';
     }
     //<!-- this id name must use unique key-->
     render() {
@@ -19,6 +33,7 @@ class Bar extends React.Component {
             <div id="topBar">
                 <div class="bar"  onClick={this.clickButton1} id="Scene1"><div>Scene1</div> <div id="barclose">×</div></div>
                 <div class="bar" onClick={this.clickButton2} id="code1"><div>code1.R</div> <div id="barclose">×</div></div>
+                <div class="bar" onClick={this.clickButton3} id="texture1"><div>texture</div> <div id="barclose">×</div></div>
             </div>
         );
     }
