4209d6fafadbcc4c3554fb3dfa401d12072fb977
01miaom <63391992+01miaom@users.noreply.github.com>
2022-10-29 17:49:15 +0800

    Update material.jsx

--- components/material.jsx
+++ components/material.jsx
@@ -1,9 +1,7 @@
 function Material() {
         return (
             <div id="materialcards" >
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-circle-fill" viewBox="0 0 16 16" style={{color:'#eee'}}>
-                <circle cx="8" cy="8" r="8"/>
-                </svg>
+                 <canvas id="canvasEdited" ></canvas>
                 <p id="materialname">material</p>
             </div>
         );
