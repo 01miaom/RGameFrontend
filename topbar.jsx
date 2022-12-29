88aa5af0612fcdc49bc83747c37684a61267eb43
01miaom <63391992+01miaom@users.noreply.github.com>
2022-12-30 02:22:24 +0800

    Update topbar.jsx

--- topbar.jsx
+++ topbar.jsx
@@ -2,7 +2,8 @@
 class Bar extends React.Component {
      clickButton1(){
         console.log("change to maincanvas");
-         document.getElementsByClassName('codeEditor')[0].style.visibility = 'hidden';
+        document.getElementsByClassName('codeEditor')[0].style.visibility = 'hidden';
+         document.getElementById('runResult0').style.visibility = 'hidden';
         document.getElementById('texture').style.visibility = 'hidden';
         document.getElementById('mainCanvas').style.visibility = 'visible';
         document.getElementById('Scene1').style.boxShadow = '0 0 4px white';
@@ -12,6 +13,7 @@ class Bar extends React.Component {
      clickButton2(){
         console.log("change to codeeditor");
         document.getElementsByClassName('codeEditor')[0].style.visibility = 'visible';
+         document.getElementById('runResult0').style.visibility = 'hidden';
         document.getElementById('texture').style.visibility = 'hidden';
         document.getElementById('mainCanvas').style.visibility = 'hidden';
         document.getElementById('code1').style.boxShadow = '0 0 4px white';
@@ -21,7 +23,18 @@ class Bar extends React.Component {
     clickButton3(){
         console.log("change to texture");
         document.getElementById('texture').style.visibility = 'visible';
-         document.getElementsByClassName('codeEditor')[0].style.visibility = 'hidden';
+        document.getElementById('runResult0').style.visibility = 'hidden';
+        document.getElementById('codeEditor').style.visibility = 'hidden';
+        document.getElementById('mainCanvas').style.visibility = 'hidden';
+        document.getElementById('code1').style.boxShadow = '0 0 0 0 white';
+         document.getElementById('Scene1').style.boxShadow = '0 0 0 0 white';
+        document.getElementById('texture1').style.boxShadow = '0 0 4px white';
+    }
+    clickButton4(){
+        console.log("change to texture");
+        document.getElementById('texture').style.visibility = 'hidden';
+        document.getElementById('runResult0').style.visibility = 'visible';
+        document.getElementById('codeEditor').style.visibility = 'hidden';
         document.getElementById('mainCanvas').style.visibility = 'hidden';
         document.getElementById('code1').style.boxShadow = '0 0 0 0 white';
          document.getElementById('Scene1').style.boxShadow = '0 0 0 0 white';
@@ -38,6 +51,8 @@ class Bar extends React.Component {
                 })}
 
                 <div class="bar" onClick={this.clickButton3} id="texture1"><div>texture</div> <div id="barclose">×</div></div>
+
+               <div class="bar" onClick={this.clickButton4} id="run"><div>run</div> <div id="barclose">×</div></div>
             </div>
         );
     }
