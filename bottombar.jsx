fe8267fb192c9e94ead582a41b0aa1581e0999c4
01miaom <63391992+01miaom@users.noreply.github.com>
2023-03-17 03:46:29 +0800

    Update bottombar.jsx

--- bottombar.jsx
+++ bottombar.jsx
@@ -2,12 +2,12 @@ let currentDocumentTree = [
     {father: ".", name:"texture", type:"Folder", id:"1", color:"#333",line:1},
     {father: ".", name:"test.jpg", type:"Image", id:"2", color:"#333",line:1},
     {father: "1", name:"test2.jpg", type:"Material", id:"3", color:"#333",line:1},
-    {father: ".", name:"test.jpg", type:"Image", id:"2", color:"#333",line:1},
-    {father: "1", name:"test2.jpg", type:"Video", id:"3", color:"#333",line:1},
-    {father: ".", name:"test.jpg", type:"Image", id:"2", color:"#333",line:1},
-    {father: "1", name:"test2.jpg", type:"Model", id:"3", color:"#333",line:1},
-    {father: ".", name:"test.jpg", type:"Image", id:"2", color:"#333",line:1},
-    {father: "1", name:"test2.jpg", type:"Image", id:"3", color:"#333",line:1}
+    {father: ".", name:"test.jpg", type:"Image", id:"4", color:"#333",line:1},
+    {father: "1", name:"test2.jpg", type:"Video", id:"5", color:"#333",line:1},
+    {father: ".", name:"test.jpg", type:"Image", id:"6", color:"#333",line:1},
+    {father: "1", name:"test2.jpg", type:"Model", id:"7", color:"#333",line:1},
+    {father: ".", name:"test.jpg", type:"Image", id:"8", color:"#333",line:1},
+    {father: "1", name:"test2.jpg", type:"Image", id:"9", color:"#333",line:1}
 ];
 
 
@@ -16,17 +16,24 @@ class BottonBarAssets extends React.Component {
     render() {
         return (
             <div id="assetsPage">
-                <div id="line">
-                <Material />
-                <Image />
-                <Audio />
-                <Folder />
-                <Video />
-                <Model />
-                    </div>
-                <div id="line">
-                <Video />
-                </div>
+                {currentDocumentTree.map((currentDocumentTree)=>{
+                        switch(currentDocumentTree.type){
+                            case "Folder":
+                                return<Folder key={currentDocumentTree.id} />;
+                            case "Material":
+                                return<Material key={currentDocumentTree.id } />;
+                            case "Image":
+                                return<Image key={currentDocumentTree.id } />;
+                            case "Model":
+                                return<Model key={currentDocumentTree.id } />;
+                            case "Video":
+                                return<Video key={currentDocumentTree.id } />;
+                            case "Audio":
+                                return<Audio key={currentDocumentTree.id } />;
+                            default:
+                                return null;
+                        }
+                    })}
             </div>
         );
     }
